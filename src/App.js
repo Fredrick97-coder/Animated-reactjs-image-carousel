7fd8fbc0ab0a96582fcab850aa12960425fbbc35
Fredrick97-coder <fredrickotabil97@gmail.com>
2022-07-04 17:12:15 +0000

    this is your fancy reactjs image caroussel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import { Gallery } from './models/Gallery';
+import { RiArrowGoBackFill, RiArrowGoForwardLine } from 'react-icons/ri';
+import { motion } from 'framer-motion';
 
 function App() {
+  const [selected, setSelected] = useState(0);
+  const lGallery = Gallery.length;
+
+  const transition = { type: 'spring', duration: 2 };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative h-screen bg-slate-300">
+      <div className="relative overflow-hidden">
+        <motion.img
+          key={Gallery[selected].id}
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={transition}
+          src={Gallery[selected].img}
+          alt={Gallery[selected].title}
+          className="h-screen w-screen object-cover cursor-pointer opacity-50 hover:opacity-100 hover:cursor-pointer"
+        />
+        <div className="absolute top-80 left-0 right-0 bottom-50 ">
+          <div className="flex flex-col justify-center items-center">
+            <h3 className="text-[4rem] text-white font-semibold">
+              {Gallery[selected].title}
+            </h3>
+            <h3
+              className="text-black text-center text-xl font-medium w-[25rem] bg-slate-200 p-2 opacity-50"
+              numberOfLines={2}
+            >
+              {Gallery[selected].description}
+            </h3>
+          </div>
+        </div>
+        <div className="absolute bottom-10 left-0 right-0 flex justify-center items-center">
+          <div
+            className="bg-white p-5 mx-10 rounded-md cursor-pointer hover:scale-110 transition-all"
+            onClick={() => {
+              selected === 0
+                ? setSelected(lGallery - 1)
+                : setSelected(selected - 1);
+            }}
+          >
+            <RiArrowGoBackFill />
+          </div>
+          <div
+            className="bg-white p-5 mx-10 rounded-md cursor-pointer hover:scale-110 transition-all"
+            onClick={() => {
+              selected === lGallery - 1
+                ? setSelected(0)
+                : setSelected(selected + 1);
+            }}
+          >
+            <RiArrowGoForwardLine />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/models/Gallery.jsx
@@ -0,0 +1,27 @@
+import image1 from '../assets/foto-sushi-6anudmpILw4-unsplash.jpg';
+import image2 from '../assets/gregory-hayes-h5cd51KXmRQ-unsplash(1).jpg';
+import image3 from '../assets/lethicia-matos-CSh-hNHc3Y8-unsplash.jpg';
+
+export const Gallery = [
+  {
+    id: 1,
+    img: image1,
+    title: 'Foto Sushi',
+    description:
+      'Sushi is a Japanese dish of raw fish, usually sashimi, that is eaten raw.',
+  },
+  {
+    id: 2,
+    img: image2,
+    title: 'Gregory Hayes',
+    description:
+      'Gregory is a fictional character in the Mario video game series. He is a plumber who lives in the Mushroom Kingdom.',
+  },
+  {
+    id: 3,
+    img: image3,
+    title: 'Lethicia Matos',
+    description:
+      'Lethicia is a fictional character in the Mario video game series. She is a plumber who lives in the Mushroom Kingdom.',
+  },
+];
